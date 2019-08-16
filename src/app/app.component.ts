e84bac0ffcecdae6add93a462e864cc832bc30ba
Kgothatso Ditshwene <kgothatsod@bcx.co.za>
2019-08-16 20:15:33 +0200

    Clients and rentals lists coming alright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hire-trailer';
+
+  clients = [{
+    name: "Tshego",
+    lastname: "Sebata",
+    email: "tshego@domain.com"
+  },
+  {
+      name: "shade",
+      lastname: "Samba",
+      email: "shade@domain.com"
+  }];
+
+  rentals = [{
+    client: "Tshego Sebata",
+    date: "2019-07-02 15:44:10.830",
+    trailer: "CP 55 VV GP"
+  },
+  {
+    client: "shade Samba",
+    date: "2019-07-02 15:44:10.830",
+    trailer: "CP 55 VV GP"
+  }];
 }
--- src/pages/item-list.ts
+++ src/pages/item-list.ts
@@ -3,9 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'item-list',
     templateUrl: './item-list.html',
+    styleUrls: ['./item-list.css']
 })
 
 export class ItemListComponent {
-    title = 'hire-trailer';
-    @Input() name: string;
+    @Input() title: string;
+    @Input() items;
+    @Input() currentTab: string;
 }
